cc0d0efed8bade3a9287991cdc86671ccea6942b
Mdyachkovskiy <mdyachkovskiy@gmail.com>
2023-01-03 18:38:44 +0500

    HW_5 Initial commit

--- app/src/main/java/com/example/kotlin_movieapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_movieapp/MainActivity.kt
@@ -6,7 +6,7 @@ import com.example.kotlin_movieapp.databinding.ActivityMainBinding
 import com.example.kotlin_movieapp.ui.main.MovieListFragment
 
 class MainActivity : AppCompatActivity() {
-
+//Homework 5
     private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState : Bundle?) {
