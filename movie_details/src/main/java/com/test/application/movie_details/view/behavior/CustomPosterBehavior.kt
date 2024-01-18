0d73f0a9c76984b26ea9f71afd1ceb77e09c020e
MDyachkovskiy <115916173+MDyachkovskiy@users.noreply.github.com>
2024-01-18 18:09:34 +0500

    - ошибка в movie_details_fragment

--- movie_details/src/main/java/com/test/application/movie_details/view/behavior/CustomPosterBehavior.kt
+++ movie_details/src/main/java/com/test/application/movie_details/view/behavior/CustomPosterBehavior.kt
@@ -2,15 +2,22 @@ package com.test.application.movie_details.view.behavior
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import androidx.cardview.widget.CardView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.google.android.material.appbar.AppBarLayout
 import com.test.application.movie_details.R
+import kotlin.math.abs
 
 class CustomPosterBehavior(
     context: Context, attrs: AttributeSet
 ) : CoordinatorLayout.Behavior<CardView>(context, attrs) {
+
+    private var isAppBarLayoutScrolled = false
+
+    private var lastAppBarLayoutY = Float.MAX_VALUE
+    private var threshold = 10f
 
     private var originalHeight = 0f
     private var originalWidth = 0f
@@ -44,27 +51,57 @@ class CustomPosterBehavior(
         dependency: View
     ): Boolean {
         if (dependency is AppBarLayout) {
-            val ratio = (dependency.totalScrollRange + dependency.y) / dependency.totalScrollRange
-
-            val newWidth = (originalWidth * (0.5f + ratio * 0.5f)).toInt()
-            val newHeight = (originalHeight * (0.5f + ratio * 0.5f)).toInt()
-            child.layoutParams.width = newWidth
-            child.layoutParams.height = newHeight
-
-            val targetX = parent.width - newWidth - parent.resources
-                .getDimensionPixelSize(com.test.application.core.R.dimen.margin_16dp_medium)
-
-            val targetY = toolbarHeight + parent.resources
-                .getDimensionPixelSize(com.test.application.core.R.dimen.margin_8dp_small)
-
-            val newX = originalX - (originalX - targetX) * (1 - ratio)
-            val newY = originalY - (originalY - targetY) * (1 - ratio)
-            child.x = newX.coerceAtLeast(targetX.toFloat())
-            child.y = newY.coerceAtLeast(targetY.toFloat())
-
-            child.requestLayout()
+            return handleAppBarLayoutChange(parent, child, dependency)
         }
         return true
+    }
+
+    private fun handleAppBarLayoutChange(
+        parent: CoordinatorLayout, child: CardView, appBarLayout: AppBarLayout
+    ) : Boolean {
+        val appBarLayoutY = appBarLayout.y
+
+        if (!isAppBarLayoutScrolled && appBarLayoutY != 0f) {
+            isAppBarLayoutScrolled = true
+        }
+
+        if(isAppBarLayoutScrolled && shouldUpdatePosition(appBarLayoutY)){
+            updateCardViewPosition(parent, child, appBarLayout)
+            lastAppBarLayoutY = appBarLayoutY
+        }
+        return true
+    }
+
+    private fun updateCardViewPosition(parent: CoordinatorLayout, child: CardView, appBarLayout: AppBarLayout) {
+        val ratio = (appBarLayout.totalScrollRange + appBarLayout.y) / appBarLayout.totalScrollRange
+
+        val newWidth = (originalWidth * (0.5f + ratio * 0.5f)).toInt()
+        val newHeight = (originalHeight * (0.5f + ratio * 0.5f)).toInt()
+        child.layoutParams.width = newWidth
+        child.layoutParams.height = newHeight
+
+        val targetX = parent.resources
+            .getDimensionPixelSize(com.test.application.core.R.dimen.margin_16dp_medium)
+
+        val targetY = toolbarHeight + parent.resources
+            .getDimensionPixelSize(com.test.application.core.R.dimen.margin_8dp_small)
+
+        val newX = originalX - (originalX - targetX) * (1 - ratio)
+        val newY = if (ratio < 1f) {
+            originalY + (targetY - originalY) * (1 - ratio)
+        } else {
+            originalY
+        }
+
+        child.x = newX.coerceAtLeast(targetX.toFloat())
+        child.y = newY.toFloat()
+
+        Log.d("@@@", "Updated X: ${child.x}, Y: ${child.y}")
+        child.requestLayout()
+    }
+
+    private fun shouldUpdatePosition(appBarLayoutY: Float): Boolean {
+        return abs(lastAppBarLayoutY - appBarLayoutY) > threshold
     }
 
     private fun initialize(child: CardView, parent: CoordinatorLayout) {
@@ -72,15 +109,20 @@ class CustomPosterBehavior(
         originalWidth = child.measuredWidth.toFloat()
 
         val anchorView = parent.findViewById<View>(R.id.background_image_block)
+        val anchorHeight = anchorView.measuredHeight
 
-        val anchorLocation = IntArray(2)
-        anchorView.getLocationOnScreen(anchorLocation)
+        originalY = anchorHeight - child.measuredHeight
 
-        originalX = anchorLocation[1] + parent.resources
+        val marginStart = parent.resources
             .getDimensionPixelSize(com.test.application.core.R.dimen.margin_24dp_medium)
-        originalY = anchorLocation[1] - child.measuredHeight
+        originalX = marginStart
+
+        child.x = originalX.toFloat()
+        child.y = originalY.toFloat()
 
         toolbarHeight = parent.findViewById<View>(R.id.toolbar).height
         isInitialize = true
+
+        Log.d("@@@", "Initialized with X: $originalX, Y: $originalY")
     }
 }
